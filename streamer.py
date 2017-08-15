cc4295405b8415dd203a992d9042228c6e3c7e66
root <root@gtone.playground>
2017-08-14 23:11:29 -0400

    weather integration

--- streamer.py
+++ streamer.py
@@ -14,6 +14,7 @@ consumer_key = config.get('TwitterCodes','consumerkey')
 consumer_secret = config.get('TwitterCodes','consumerkeysecret')
 splunk_server=config.get('Splunk','server')
 splunk_hec=config.get('Splunk','HECToken')
+weatherkey=config.get('weather','key')
 headers=headers = {'Authorization': 'Splunk '+splunk_hec}
 #This is a basic listener that just prints received tweets to stdout.
 
@@ -22,13 +23,34 @@ def sendtoHEC(mydata):
 	mydata={"event":mydata}
 	r = requests.post('https://'+splunk_server+':8088/services/collector', data=json.dumps(mydata), headers=headers, verify=False)
 	print r
+
+def getWeather(city,state):
+	urlstring="http://api.wunderground.com/api/"+weatherkey+"/conditions/q/"+state+"/"+city+".json"
+	print urlstring
+	r=requests.get(urlstring)
+	print r.content
+	return r.content
 class StdOutListener(StreamListener):
 
     def on_data(self, data):
         print data
 	sendtoHEC(data)
-        return True
-
+	#get location info
+	try:
+		location=json.loads(data)	
+		location=location['place']['full_name']
+		print location
+		locationarray=location.split(",")
+		print locationarray
+		weatherinfo = getWeather(locationarray[0].strip(),locationarray[1].strip())
+		weatherinfo=json.loads(weatherinfo)
+		weatherinfo=weatherinfo['current_observation']
+        	print weatherinfo
+		sendtoHEC(weatherinfo)
+		print location
+	except:
+		print "no location data"
+	return True
     def on_error(self, status):
         print status
 
